65287b3bd7b47ce5eb7279a0602cb8aaf8da8b7c
jonspandorf <jonspandorf@gmail.com>
2022-05-07 13:24:21 +0000

    fixed var files in ecr

--- create_ecr/vars.tf
+++ create_ecr/vars.tf
@@ -1,16 +1,4 @@
 
-variable "AWS_SECRET_KEY" {
-
-}
-
-variable "AWS_ACCESS_KEY" {
-
-}
-
-variable "ecr_source" {
-    type = string
-    default = "/home/jon/mobileyeProject/terraform/modules/aws/ecr"
-}
 
 variable "my_ecr" {
   type = string 
